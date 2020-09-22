004d289373b4a82743cf490f0921db1500ba8bcb
Himanshu Gautam <1302399+hgautam@users.noreply.github.com>
2020-09-21 17:27:44 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import "fmt"
+import "time"
+import "os"
 
-// this is a comment
 
 func main() {
-    fmt.Println("Hello World")
+    fmt.Println("Hello from your Docker build agent!!")
+    currentTime := time.Now()
+    fmt.Println("Current time and date: ", currentTime.Format("2006.01.02 15:04:05"))
+    name, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+    fmt.Println("Container hostname:", name)
 }
